28e22caf51d5ab48af958a934f06a9c28974b131
Muhammad Rizal Nurfirdaus <M.Rizal.Nurfirdaus@outlook.com>
2025-06-29 10:36:29 +0700

    Hopefully there won't be any more problems with my XAMPP.

--- Modul_9/AplikasiPenjualan/src/app/koneksi/Koneksi.java
+++ Modul_9/AplikasiPenjualan/src/app/koneksi/Koneksi.java
@@ -1,27 +1,22 @@
 package app.koneksi;
 
-import com.mysql.cj.jdbc.MysqlDataSource;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Koneksi {
     static Connection con;
 
-    public static Connection connection() {
+  public static Connection connection() {
         if (con == null) {
             MysqlDataSource data = new MysqlDataSource();
-            data.setServerName("localhost"); // Terhubung ke komputer sendiri
-            data.setPortNumber(3307); // Port yang diekspos oleh Docker
-            data.setDatabaseName("db_penjualan_toko"); // Nama DB dari init.sql
-            data.setUser("root"); // User dari docker-compose.yml
-            data.setPassword("root"); // Password dari docker-compose.yml
-
+            data.setDatabaseName("db_penjualan_toko");
+            data.setUser("root");
+            data.setPassword("");
             try {
                 con = data.getConnection();
-                System.out.println("Koneksi berhasil!");
             } catch (SQLException e) {
-                System.err.println("Koneksi Gagal! Error: " + e.getMessage());
-                e.printStackTrace(); // Cetak detail error untuk debugging
+                System.out.println("Koneksi Gagal : " + e.getMessage());
             }
         }
         return con;
